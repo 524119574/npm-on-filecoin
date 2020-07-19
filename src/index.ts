38ade8411fef2639e11f4846d623a46224802246
Leonard Ge <wge@confluent.io>
2020-07-19 17:51:23 +0100

    Set up project.

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+#! /usr/bin/env node
+
+'use strict'
+
+import {Options, startServer} from "./server/server"
+
+require('dnscache')({ enable: true })
+
+const rc = require('rc')
+const { spawn } = require('child_process')
+
+const yargs = require('yargs').config(rc("npm-on-filecoin", null, {}));
+
+const cleanUpOps : Array<() => Promise<void>> = []
+
+const cleanUp = () => {
+  Promise.all(
+    cleanUpOps.map(op => op())
+  ).then(() => process.exit(0))
+}
+
+process.on('SIGTERM', cleanUp)
+process.on('SIGINT', cleanUp)
+
+
+
+const proxyCommand = async (options: Options) => {
+  // options = config(options)
+
+  console.info('👩‍🚀 Starting local proxy') // eslint-disable-line no-console
+
+  const server = await startServer(options)
+
+  cleanUpOps.push(() => {
+    return new Promise((resolve) => {
+      server.close(() => {
+        console.info('✋ Server stopped') // eslint-disable-line no-console
+        resolve()
+      })
+    })
+  })
+
+  const packageManager = '/usr/local/bin/npm'
+
+  console.info(`🎁 Installing dependencies with ${packageManager}`) // eslint-disable-line no-console
+  const setRegistry = `--registry=http://localhost:${options.httpPort}`;
+
+  const proc = spawn(packageManager, [setRegistry].concat(process.argv.slice(2)), {
+    stdio: 'inherit'
+  })
+
+  proc.on('close', async (code:number) => {
+    console.log(`🎁 ${packageManager} exited with code ${code}`) // eslint-disable-line no-console
+
+    // await rewriteLockfile(options)
+    // await cleanUp()
+
+    process.exit(code)
+  })
+
+}
+
+const setUpOptions = (yargs: any) => { // eslint-disable-line no-unused-expressions
+    yargs
+      .option('ipfs-registry', {
+        describe: 'Where to download manifest if it is not in local MFS.',
+        default: 'https://registry.js.ipfs.io',
+        type: 'string',
+      })
+}
+
+
+yargs.command('$0', 'Installs your js dependencies using IPFS and Filecoin', setUpOptions, proxyCommand).argv;
+
+
+
+
---
+++ src/server/manifest.ts
@@ -0,0 +1,96 @@
+'use strict'
+
+import {Options} from "../server/server";
+import fetch from 'node-fetch';
+
+// interface Package {
+//     name: string;
+//     versions: Map<Number, string>;
+
+// }
+
+// const replaceTarballUrls = (pkg: any, config: Options):any => {
+//   const prefix = `http://localhost:${config.httpPort}`
+//   const packageName = pkg.name
+//   const moduleName = packageName.startsWith('@') ? packageName.split('/').pop() : packageName
+
+//   // change tarball URLs to point to us
+//   Object.keys(pkg.versions || {})
+//     .forEach(versionNumber => {
+//       const version = pkg.versions[versionNumber]
+
+//       version.dist.tarball = `${prefix}/${packageName}/-/${moduleName}-${versionNumber}.tgz`
+//     })
+
+//   return pkg
+// }
+
+export const manifest = (config: Options, _app: any) => {
+  return async (request:any, response:any, _next:any) => {
+    console.log("manifest", request.path)
+
+    let moduleName = sanitiseName(request.path)
+
+    console.info(`Loading manifest for ${moduleName}`)
+
+    try {
+      const manifest = await loadManifest(config, moduleName)
+
+      // because we start the server on a random high port, the previously stored
+      // manifests may have port numbers from the last time we ran, so overwrite
+      // them before returning them to the client
+      // replaceTarballUrls(manifest, config)
+      
+      // console.log('amni', manifest)
+      response.statusCode = 200
+      response.setHeader('Content-type', 'application/json; charset=utf-8')
+      response.send(JSON.stringify(manifest, null, request.query.format === undefined ? 0 : 2))
+    } catch (error) {
+      console.error(`💥 Could not load manifest for ${moduleName}`, error) // eslint-disable-line no-console
+
+      if (error.message.includes('Not found')) {
+        response.statusCode = 404
+        console.info("package not found")
+
+        return
+      }
+
+      // a 500 will cause the npm client to retry
+      response.statusCode = 500
+      response.send(lol(`💥 ${error.message}`))
+    }
+  }
+}
+
+const lol = (message: string) => {
+    return `<marquee><font size=50>${message}</font></marquee>`
+}
+
+
+const sanitiseName = (name:string) : string => {
+  name = `${(name || '').trim()}`.replace(/^(\/)+/, '/')
+
+  if (name.startsWith('/')) {
+    name = name.substring(1)
+  }
+  
+  // Revert the URL encoding for `/`
+  if (name.startsWith('@')) {
+    name = name.replace(/%2f/g, '/')
+  }
+
+  return name
+}
+
+const loadManifest = async (_options: Options, _packageName:string) : Promise<any> => {
+  (async () => {
+    const response = await fetch('https://registry.js.ipfs.io/' + _packageName);
+    const json = await response.json();
+  
+    console.log(json);
+
+    return json;
+  })();
+
+  return null
+}
